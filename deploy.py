23df7c1d4bfd4e2da05f550215fa4c836c6b273b
Maxim Y. Popov <maxpmy@gmail.com>
2019-08-24 00:18:41 -0400

    Yabai config added

--- deploy.py
+++ deploy.py
@@ -4,20 +4,20 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 HOME_DIR = os.path.expanduser('~')
 
 # Create symlinks for config files:
-for config_type in ['vim', 'tmux']:
+for config_type in ['vim', 'tmux', 'yabai']:
     dirname = os.path.join(ROOT_DIR, config_type)
     for filename in os.listdir(dirname):
         linkname = os.path.join(HOME_DIR, filename)
         filename = os.path.join(dirname, filename)
         if os.path.exists(linkname):
-            print "[{}] Symlink already exists: {}".format(config_type, filename, linkname)
+            print("[{}] Symlink already exists: {}".format(config_type, filename, linkname))
         else:
             os.symlink(filename, linkname)
-            print "[{}] Linked: {} -> {}".format(config_type, filename, linkname)
+            print("[{}] Linked: {} -> {}".format(config_type, filename, linkname))
 
 # Clone Vundle
 vundle_path = os.path.join(HOME_DIR, ".vim", "bundle", "Vundle.vim")
 cmd = "git clone https://github.com/VundleVim/Vundle.vim.git {}".format(vundle_path)
-print "[Vundle] {}".format(cmd)
+print("[Vundle] {}".format(cmd))
 os.system(cmd)
 
