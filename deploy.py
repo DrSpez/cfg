fd6bc9d6a5441554b568d6050be196e35a2d9220
Maxim Popov <maxim.popov@blinkhealth.com>
2019-08-28 15:52:46 -0400

    skhd config for yabai hotkeys

--- deploy.py
+++ deploy.py
@@ -2,9 +2,10 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 HOME_DIR = os.path.expanduser('~')
+APPS = ['vim', 'tmux', 'yabai', 'skhd']
 
 # Create symlinks for config files:
-for config_type in ['vim', 'tmux', 'yabai']:
+for config_type in APPS:
     dirname = os.path.join(ROOT_DIR, config_type)
     for filename in os.listdir(dirname):
         linkname = os.path.join(HOME_DIR, filename)
@@ -20,4 +21,3 @@ vundle_path = os.path.join(HOME_DIR, ".vim", "bundle", "Vundle.vim")
 cmd = "git clone https://github.com/VundleVim/Vundle.vim.git {}".format(vundle_path)
 print("[Vundle] {}".format(cmd))
 os.system(cmd)
-
