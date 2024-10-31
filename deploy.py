69b528185accf5d02d8d419ce46a011f7aae5e67
Maxim Y. Popov <maxpmy@gmail.com>
2024-10-31 01:11:30 -0400

    add nvim custom links in deploy script

--- deploy.py
+++ deploy.py
@@ -2,19 +2,29 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 HOME_DIR = os.path.expanduser('~')
-APPS = ['vim', 'tmux', 'yabai', 'skhd']
+CONFIG_DIRS = ['vim', 'tmux', 'yabai', 'skhd']
 
 # Create symlinks for config files:
-for config_type in APPS:
-    dirname = os.path.join(ROOT_DIR, config_type)
+for config_dir in CONFIG_DIRS:
+    dirname = os.path.join(ROOT_DIR, config_dir)
     for filename in os.listdir(dirname):
         linkname = os.path.join(HOME_DIR, filename)
         filename = os.path.join(dirname, filename)
         if os.path.exists(linkname):
-            print("[{}] Symlink already exists: {}".format(config_type, filename, linkname))
+            print("[{}] Symlink already exists: {}".format(config_dir, filename, linkname))
         else:
             os.symlink(filename, linkname)
-            print("[{}] Linked: {} -> {}".format(config_type, filename, linkname))
+            print("[{}] Linked: {} -> {}".format(config_dir, filename, linkname))
+
+# Symlink vim configs to nvim dirs also:
+filename = os.path.join(ROOT_DIR, 'vim', '.vimrc')
+linkname = os.path.join(HOME_DIR, '.config', 'nvim', 'init.vim')
+os.symlink(filename, linkname)
+print("[nvim] Linked: {} -> {}".format(filename, linkname))
+filename = os.path.join(ROOT_DIR, 'vim', '.vim', 'colors')
+linkname = os.path.join(HOME_DIR, '.config', 'nvim', 'colors')
+os.symlink(filename, linkname)
+print("[nvim] Linked: {} -> {}".format(filename, linkname))
 
 # Clone Vundle
 vundle_path = os.path.join(HOME_DIR, ".vim", "bundle", "Vundle.vim")
