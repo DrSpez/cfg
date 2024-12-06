c531e2fae9d803073460b08b081e28159a8ce164
Maxim Y. Popov <maxpmy@gmail.com>
2024-12-05 23:59:36 -0500

    configure python lsp; add neotree plugin

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -27,7 +27,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set({'n', 'x'}, 'gf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
     end,
 })
@@ -39,6 +39,18 @@ require('lspconfig').ts_ls.setup({ on_attach = function(client, bufnr)
     print('[lspconfig]: attached ts_ls')
 end,
 })
+
+require('lspconfig').pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          maxLineLength = 72
+        }
+      }
+    }
+  }
+}
 
 require('lspconfig').omnisharp.setup({
     capabilities = capabilities,
