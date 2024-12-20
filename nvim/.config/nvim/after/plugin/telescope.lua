eb975018d582cc21dfdd5729e8e02a663d9b25be
DrSpez <maxim.popov@talkspace.com>
2024-12-20 13:02:04 -0500

    telescope ignore node_modules, .git

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,11 @@
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = { "node_modules", ".git/" },
+    },
+}
+
 local builtin = require('telescope.builtin')
+
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope fuzzy search through output of git ls-files. Respects .gitignore' })
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
