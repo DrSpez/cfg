ac402bfce57f480bc4e4ab7e210635cdcdd76df2
DrSpez <maxim.popov@talkspace.com>
2025-07-10 18:19:20 -0400

    follow opened file in neotree

--- nvim/.config/nvim/lua/maxpy/plugins.lua
+++ nvim/.config/nvim/lua/maxpy/plugins.lua
@@ -43,6 +43,20 @@ return {
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        opts = {
+            filesystem = {
+                -- This option finds the file when you open Neo-tree
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true,
+                },
+                -- This option keeps Neo-tree synced as you switch buffers
+                --update_focused_file = {
+                --    enable = true,
+                --    update_root = true, -- This is optional, but recommended
+                --},
+            }
         }
     },
 
