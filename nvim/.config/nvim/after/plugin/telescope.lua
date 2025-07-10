e83aa0df317f4372f22e16d2e92dad0850fe9821
DrSpez <maxim.popov@talkspace.com>
2025-07-10 17:09:00 -0400

    telescope better search/file search config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,14 +1,34 @@
 require('telescope').setup{
     defaults = {
         file_ignore_patterns = { "node_modules", ".git/" },
+        vimgrep_arguments = {
+            'rg', -- NOTE: ripgrep is required
+            '--column',
+            '--smart-case',
+            '--hidden',
+            '--follow'
+        },
+        layout_strategy = 'horizontal',
     },
 }
 
 local builtin = require('telescope.builtin')
 
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope fuzzy search through output of git ls-files. Respects .gitignore' })
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end, { desc = 'Grep in the project directory. Requires ripgrep installation.' })
+vim.keymap.set('n', '<leader>p', function()
+    builtin.find_files({
+        hidden=true,
+    })
+end, { desc = 'Fuzzy-find files. Respects .gitignore' })
+
+vim.keymap.set('n', '<leader>F', function()
+    builtin.live_grep({
+        additional_args = { '--case-sensitive' },
+    })
+end, { desc = 'Find text, regex, case sensitive' })
+
+vim.keymap.set('n', '<leader>f', function()
+    builtin.live_grep({
+        additional_args = { '--smart-case' }
+    })
+end, { desc = 'Find text, case insensitive' })
