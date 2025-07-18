4e6f8e7d18661c23ba01f9e9bc21e9a13a6c3c13
DrSpez <maxim.popov@talkspace.com>
2025-07-18 19:22:37 -0400

    ts/tsreact setup working

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -2,7 +2,9 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     javascript = { "biome", "prettier", stop_at_first = true },
-    typescript = { "biome" },
+    typescript = { "biome", "prettier", stop_at_first = true },
+    typescriptreact = { "prettier", "biome", stop_at_first = true },
+    ["_"] = { "lsp" }, -- default everything else to LSP formatter
   },
   formatters = {
     stylua = {
@@ -14,31 +16,38 @@ require("conform").setup({
       },
     },
     biome = {
-      command = function(self, ctx)
-        -- vim.fs.find searches upward from the current buffer's path
-        local biome_executable = vim.fs.find({
-          "node_modules/.bin/biome",
-        }, {
+      -- Statically define the command to use the local executable
+      command = "node_modules/.bin/biome",
+      -- Add a condition to check if the local executable exists before running
+      condition = function(ctx)
+        return vim.fs.find({ "node_modules/.bin/biome" }, {
           path = ctx.dirname,
           upward = true,
           limit = 1,
           type = "file",
         })[1]
-        -- If a local executable is found, return its full path.
-        -- Otherwise, return the default "biome" command.
-        return biome_executable or "biome"
       end,
       args = {
         "format",
-        "--skip-errors",
         "--stdin-file-path",
         "$FILENAME",
         "--fix",
       },
     },
+    prettier = {
+      command = "prettier",
+      -- Conform will run this command to determine if the formatter is available.
+      -- If it is not, it will not be used.
+      condition = function(ctx)
+        return vim.fs.find({ "node_modules/.bin/prettier" }, {
+          path = ctx.dirname,
+          upward = true,
+          limit = 1,
+        })[1]
+      end,
+    },
   },
   format_on_save = {
     timeout_ms = 500,
-    lsp_format = "fallback",
   },
 })
