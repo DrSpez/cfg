77232f1bf50767750617a156a27fb0fd139d8029
Maxim Y. Popov <maxpmy@gmail.com>
2025-07-27 20:58:58 -0400

    harpoon - bind <leader>{number} to open file by number

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -5,6 +5,19 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function()
   harpoon:list():add()
 end)
+
 vim.keymap.set("n", "<leader>e", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+  harpoon.ui:toggle_quick_menu(harpoon:list(), {
+    title = " Important Files ",
+    title_pos = "center",
+    ui_width_ratio = 0.50,
+    border = "rounded",
+  })
 end)
+
+-- <leader>i to open file i
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, function()
+    harpoon:list():select(i)
+  end, { desc = "Go to Harpoon file #" .. i })
+end
