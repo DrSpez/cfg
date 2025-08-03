7142ffb5f0b27225e446ceecc2604abd2025aad6
DrSpez <maxpmy@gmail.com>
2025-08-03 13:08:21 -0400

    only close LSP-related windos on BufLeave

--- nvim/.config/nvim/lua/maxpy/autocmds.lua
+++ nvim/.config/nvim/lua/maxpy/autocmds.lua
@@ -3,16 +3,20 @@ local customAuGroup = vim.api.nvim_create_augroup("CustomAutocmds", { clear = tr
 vim.api.nvim_create_autocmd("BufLeave", {
   group = customAuGroup,
   callback = function()
-    closeAllFloatingWindowsInCurrentTab()
+    closeLSPFloatingWindowsInCurrentTab() -- fixes the LSP floating windows getting stuck on harpoon buf switch
   end,
 })
 
-function closeAllFloatingWindowsInCurrentTab()
+function closeLSPFloatingWindowsInCurrentTab()
   local windows = vim.api.nvim_tabpage_list_wins(0)
   for _, winid in ipairs(windows) do
     local config = vim.api.nvim_win_get_config(winid)
-    if config.relative ~= "" then -- if floating
-      vim.api.nvim_win_close(winid, true)
+    if config.relative ~= "" then -- if floating window
+      local bufnr = vim.api.nvim_win_get_buf(winid)
+      local filetype = vim.bo[bufnr].filetype
+      if filetype == "help" or filetype == "markdown" or filetype == "lspinfo" then -- if lsp-related filetype
+        vim.api.nvim_win_close(winid, true)
+      end
     end
   end
 end
