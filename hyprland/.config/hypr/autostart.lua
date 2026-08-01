ef7677eaf16ca2cfc5f47fd06d4fb9d6f160c2f8
DrSpez <maxpmy@gmail.com>
2026-07-31 20:25:50 -0400

    hyprland: autostart gnome-keyring

--- hyprland/.config/hypr/autostart.lua
+++ hyprland/.config/hypr/autostart.lua
@@ -1,6 +1,9 @@
 hl.on("hyprland.start", function()
+  -- Essentials
   hl.exec_cmd("pgrep -x waybar >/dev/null 2>&1 && killall waybar || waybar")
   hl.exec_cmd("hyprsunset")
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("hypridle")
+  -- Secrets
+  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,ssh")
 end)
